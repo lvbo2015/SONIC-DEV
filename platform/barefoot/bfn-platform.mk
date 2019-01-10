34ebb095cd4da1ee200a77b845c252ba145e2b95
tiantianlv <38775940+tiantianlv@users.noreply.github.com>
2019-01-10 15:58:35 +0800

    Delete bfn-platform.mk

--- platform/barefoot/bfn-platform.mk
+++
@@ -1,5 +0,0 @@
-BFN_PLATFORM = bfnplatform_8.5.x.59217b4.deb
-$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_8_5/bfnplatform_8.5.x.59217b4.deb"
-
-SONIC_ONLINE_DEBS += $(BFN_PLATFORM) # $(BFN_SAI_DEV)
-$(BFN_SAI_DEV)_DEPENDS += $(BFN_PLATFORM)
