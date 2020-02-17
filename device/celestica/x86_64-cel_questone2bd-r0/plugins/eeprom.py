0bc2ad92d7ed87ffe176c0d3d800d752df16837e
Wirut Getbamrung <wgetbum@icloud.com>
2020-02-17 17:56:22 +0700

    [device/celestica]: Update questone2bd plugin (#576)
    * [device/celestica]: update questone2bd plugin
    * [platform/cel]: update questone2bd platform install script

--- device/celestica/x86_64-cel_questone2bd-r0/plugins/eeprom.py
+++ device/celestica/x86_64-cel_questone2bd-r0/plugins/eeprom.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
-
-#############################################################################
-# Celestica DX010
 #
-# Platform and model specific eeprom subclass, inherits from the base class,
-# and provides the followings:
+# Platform and model specific eeprom subclass, inherits from the base class and provides the followings:
 # - the eeprom format definition
 # - specific encoder/decoder if there is special need
-#############################################################################
+#
+
+__version__ = "0.0.1"
 
 try:
     from sonic_eeprom import eeprom_tlvinfo
 except ImportError, e:
-    raise ImportError (str(e) + "- required module not found")
+    raise ImportError(str(e) + "- required module not found")
+
+TLV_I2C = "i2c-0"
+TLV_REG = "0-0056"
+TLV_PATH = "/sys/class/i2c-adapter/{}/{}/eeprom".format(TLV_I2C, TLV_REG)
 
 
 class board(eeprom_tlvinfo.TlvInfoDecoder):
 
     def __init__(self, name, path, cpld_root, ro):
-        self.eeprom_path = "/sys/class/i2c-adapter/i2c-0/0-0056/eeprom"
+        self.eeprom_path = TLV_PATH
         super(board, self).__init__(self.eeprom_path, 0, '', True)
-
