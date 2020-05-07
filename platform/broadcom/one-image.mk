750a38323e12e6870946f2ec1527191cc6853b75
bbinxie <binxie@celestica.com>
2020-05-07 10:24:17 +0800

    add for sivlerstone

--- platform/broadcom/one-image.mk
+++ platform/broadcom/one-image.mk
@@ -27,6 +27,7 @@ $(SONIC_ONE_IMAGE)_LAZY_INSTALLS += $(DELL_S6000_PLATFORM_MODULE) \
                                $(INVENTEC_D7264Q28B_PLATFORM_MODULE) \
                                $(CEL_DX010_PLATFORM_MODULE) \
                                $(CEL_HALIBURTON_PLATFORM_MODULE) \
+                               $(CEL_SILVERSTONE_PLATFORM_MODULE)\
                                $(DELTA_AG9032V1_PLATFORM_MODULE) \
                                $(DELTA_AG9064_PLATFORM_MODULE) \
                                $(DELTA_AG5648_PLATFORM_MODULE) \
